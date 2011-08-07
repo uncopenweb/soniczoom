b2d79324b2d61881bc4ea205249eb0a0ee99d559
Robert Dallara <robertdallara@nc.rr.com>
2011-08-06 23:40:22 -0400

    Prevented shift movements and accelerations when shift key is pressed to adjust hark volume.

--- SonicZoom.js
+++ SonicZoom.js
@@ -768,35 +768,50 @@ dojo.declare("SonicZoom", null,{
 				case KEYCODE_LEFT:
 					if (!this.lfHeld) {
 						this.lfHeld = true;
-						this.ship.moveLeft();
-						this.changeCoinSound();
-						this.playLaneSound();
+						
+						if(!e.shiftKey)
+						{
+							this.ship.moveLeft();
+							this.changeCoinSound();
+							this.playLaneSound();
+						}
 					}
 					break;
 				case KEYCODE_D:
 				case KEYCODE_RIGHT: 
 					if (!this.rtHeld) {
-						//console.log("right");
 						this.rtHeld = true;
-						this.ship.moveRight();
-						this.changeCoinSound();
-						this.playLaneSound();
+						
+						if(!e.shiftKey)
+						{
+							this.ship.moveRight();
+							this.changeCoinSound();
+							this.playLaneSound();
+						}
 					}
 					break;
 				case KEYCODE_W:
 				case KEYCODE_UP:
 					if (true) {
 						this.fwdHeld = true;
-						this.ship.accelerate();
-						this.setEngineNoise();
+						
+						if(!e.shiftKey)
+						{
+							this.ship.accelerate();
+							this.setEngineNoise();
+						}
 					} 
 					break;
 				case KEYCODE_S:
 				case KEYCODE_DOWN:
 					if (true) {
 						this.dnHeld = true;
-						this.ship.deccelerate();
-						this.setEngineNoise();
+						
+						if(!e.shiftKey)
+						{
+							this.ship.deccelerate();
+							this.setEngineNoise();
+						}
 					} 
 					break;
 				case KEYCODE_ESC:
